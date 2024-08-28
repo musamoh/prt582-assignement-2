85f7ca24df8bc7a9b1bea7350fb9c648ab2d6216
musamoh <musamoh2007@gmail.com>
2024-08-28 19:36:12 +1000

    word core

--- test_scrabble.py
+++ test_scrabble.py
@@ -2,7 +2,34 @@
 This is the test file for the scrabble game
 """
 
+import pytest
 
-def test_something():
-    """This is a test function"""
-    assert (2 / 2) == 1
+from main import score
+
+
+# Test for valid score
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("a", 1),
+        ("z", 10),
+        ("dog", 5),
+        ("cat", 5),
+        ("dogcat", 10),
+        ("dog cat", 100),
+    ],
+)
+def test_score(word, expected):
+    assert score(word) == expected
+
+
+# Test for invalid score
+# Test for valid dictionary word
+# Test for invalid word
+# Test uppercase letter and lowercase letter are the same
+# Test for empty string exception
+# Test with mixed case and special characters
+# test for white space handling
+# Test length of word
+# Test for non string input
+# Test multiple words
